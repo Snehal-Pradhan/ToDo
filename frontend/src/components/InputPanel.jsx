d5bb4eb7dedf3ead7770a065bb6522e74cdca505
Snehal Pradhan <snehalpradhan23@gmail.com>
2024-10-09 18:03:50 +0000

    button added

--- frontend/src/components/InputPanel.jsx
+++ frontend/src/components/InputPanel.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export class InputPanel extends Component {
   render() {
     return (
-      <div className=' border-4 h-40 justify-center align-middle'>
-        <input className='justify-self-center'></input>
+      <div className="h-40 flex justify-center">
+        <input className="h-10 w-2/4 translate-y-5 rounded p-4 focus:ring-pink-500 align-middle border-emerald-800 text-white border bg-emerald-300 bg-opacity-10 " type="text" placeholder='Enter To Do...'/>
+        <button className='p-5 ml-5 bg-slate-200 translate-y-5 rounded h-10 align-middle'>Add</button>
       </div>
     )
   }
