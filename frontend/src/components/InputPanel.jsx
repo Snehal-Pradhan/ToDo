2b54d321cc3fc83e7b55db1582ceeb47016390ac
Snehal Pradhan <snehalpradhan23@gmail.com>
2024-10-09 18:12:31 +0000

    some changes

--- frontend/src/components/InputPanel.jsx
+++ frontend/src/components/InputPanel.jsx
@@ -5,7 +5,7 @@ export class InputPanel extends Component {
     return (
       <div className="h-40 flex justify-center">
         <input className="h-10 w-2/4 translate-y-5 rounded p-4 focus:ring-pink-500 align-middle border-emerald-800 text-white border bg-emerald-300 bg-opacity-10 " type="text" placeholder='Enter To Do...'/>
-        <button className='p-5 ml-5 bg-slate-200 translate-y-5 rounded h-10 align-middle'>Add</button>
+        <button className='p-5 ml-5 bg-slate-200 rounded h-10 align-middle'>Add</button>
       </div>
     )
   }
